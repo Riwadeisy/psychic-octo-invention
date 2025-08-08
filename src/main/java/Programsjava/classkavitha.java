ecbc0ec7b4595265fba5ceb3209229f69279bccc
Riwadeisy <--kavithavallikumar96@gmail.com>
2025-08-08 10:26:58 +0530

    new java code updated

---
+++ src/main/java/Programsjava/classkavitha.java
@@ -0,0 +1,10 @@
+package Programsjava;
+
+public class classkavitha {
+	public static void main(String[] args) {
+        String s = "java ia a programming language";
+        String[] words = s.trim().split("\\s+");
+        System.out.println("Word count: " + words.length);
+    }
+}
+
